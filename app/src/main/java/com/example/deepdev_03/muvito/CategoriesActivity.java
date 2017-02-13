e842dd260dac57baaaa61b601c9adbb316bdabba
deepdev_03 <rorudenko@gmail.com>
2017-02-13 16:34:44 +0200

    add style for categories

--- app/src/main/java/com/example/deepdev_03/muvito/CategoriesActivity.java
+++ app/src/main/java/com/example/deepdev_03/muvito/CategoriesActivity.java
@@ -1,15 +1,22 @@
 package com.example.deepdev_03.muvito;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+
+
 public class CategoriesActivity extends AppCompatActivity {
+
+    SubcategoriesFragment fragment;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +28,76 @@ public class CategoriesActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayoutMainActivity);
+        fragment = new SubcategoriesFragment();
 
-        String categories[] = {"Все объявления", "Женский гардероб","Мужской гардероб","Детские товары","Хэндмейд",
-                "Авто и мото", "Смартфоны и планшеты","Фото- и видеотехника", "Компьютерная техника","Электроника",
+        final ListView listView = (ListView) findViewById(R.id.listViewCategoriesActivity);
+        title = (TextView) findViewById(R.id.textViewToolbarCategoriesActivity);
+        ImageView imageView = (ImageView) findViewById(R.id.imageViewSelectedCategoriesItem);
+
+        final String categories[] = {"Все объявления", "Женский гардероб", "Мужской гардероб", "Детские товары", "Хэндмейд",
+                "Авто и мото", "Смартфоны и планшеты", "Фото- и видеотехника", "Компьютерная техника", "Электроника",
                 "Бытовая техника", "Для дома и дачи", "Ремонт и строительство", "Красота и здоровье", "Спорт и отдых",
                 "Хобби и развлечения", "Музыкальные инструменты", "Животные", "Услуги", "Прочее"};
 
-        for (int i = 0; i < categories.length; i++) {
 
-            TableRow row = new TableRow(this);
-            row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-            TextView textView = new TextView(this);
-            textView.setText(categories[i]);
-            textView.setTextSize(20);
-            textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-            row.addView(textView);
-            tableLayout.addView(row, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,
+                R.layout.categories_item, R.id.textViewCategoriesItem, categories);
 
-        }
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                if (position != 0) {
+
+                        title.setText(categories[position]);
+
+                        Bundle bundle = new Bundle();
+                        position--;
+                        bundle.putInt("SelectedPosition", position);
+                        SubcategoriesFragment fragment = new SubcategoriesFragment();
+                        fragment.setArguments(bundle);
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left)
+                                .add(R.id.fragment_place, fragment)
+                                .commit();
+                }
+            }
+        });
     }
 
     public void onClickBackCategories (View view){
-        finish();
+        closeActivityOrFragment ();
+    }
+
+    @Override
+    public void onBackPressed() {
+        closeActivityOrFragment ();
     }
 
 
+    public void closeActivityOrFragment (){
+
+        fragment = (SubcategoriesFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_place);
+
+        if(fragment == null ) {
+            finish();
+        } else {
+
+            title = (TextView) findViewById(R.id.textViewToolbarCategoriesActivity);
+            title.setText("Категория");
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.enter_from_left,R.anim.exit_to_right)
+                    .remove(fragment)
+                    .commit();
+        }
+    }
 
 }
+
