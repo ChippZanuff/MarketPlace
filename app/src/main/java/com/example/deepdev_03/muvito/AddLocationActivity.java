01f690d0d1decd455624190eca4c1d6c4452e7c1
ChippZanuff <zanuff32167@mail.ru>
2017-03-03 10:52:03 +0200

    Added google map in addlocationactivity

--- app/src/main/java/com/example/deepdev_03/muvito/AddLocationActivity.java
+++ app/src/main/java/com/example/deepdev_03/muvito/AddLocationActivity.java
@@ -1,20 +1,41 @@
 package com.example.deepdev_03.muvito;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class AddLocationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class AddLocationActivity extends AppCompatActivity implements OnMapReadyCallback
+{
+    private GoogleMap map;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.user_profile_settings_add_map_location);
+        mapFragment.getMapAsync(this);
     }
 
     public void onClickBackLocation (View view){
         finish();
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        this.map = googleMap;
+        LatLng tokyoUniversity = new LatLng(35.7126775, 139.76198899999997);
+        this.map.addMarker(new MarkerOptions().position(tokyoUniversity));
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(tokyoUniversity));
+    }
 }
